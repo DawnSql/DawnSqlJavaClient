d7f72263b220a2503bfcaed68dcb99c854d383a0
Cong <heian0224@gmail.com>
2022-11-26 22:39:24 +0800

    init repo.

---
+++ src/main/java/com/dawnsql/client/DawnSqlJavaClientApplication.java
@@ -0,0 +1,13 @@
+package com.dawnsql.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawnSqlJavaClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawnSqlJavaClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dawnsql/client/DawnSqlJavaClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dawnsql.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawnSqlJavaClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
